04a11d5abffcb24fc2d78f8781c0c9e4880a8e50
Gabriela-Mozer <gabrielamozer1966@gmail.com>
2024-08-28 18:02:32 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $(".slideshow-container").slick({
+    arrows: true,
+  });
+});
+
+$(document).ready(function () {
+  $(".slideshow-promos").slick({
+    arrows: true,
+    slidesToShow: 3, 
+    slidesToScroll: 1
+  });
+});
